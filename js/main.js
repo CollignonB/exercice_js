aa67ee186fc289e0a958a8a2dcd2a6a1fc35c756
Baptiste COLLIGNON <collignon.bapitste.pro@gmail.com>
2020-06-29 16:40:18 +0200

    exercice 6 done

--- js/main.js
+++ js/main.js
@@ -81,3 +81,17 @@ console.log("Une " + car["brand"] + " " + car["type"] + " de couleur "
  + car["passengers"][3]);
 
 // ~~~~~~~ Exercice 6 ~~~~~~~~  
+var a = 14;
+var b = 38;
+
+var product = [];
+
+console.log(a + " " + b + " " + product)
+
+product [0] = a;
+product [1] = b;
+product [7] = 50;
+
+console.log(product);
+
+// le tableau a été rempli de "cases" vides 
